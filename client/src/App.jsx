4a24bc5506b30f31649eeba1bef9bfa2df53f91d
jainex17 <jainexp017@gmail.com>
2024-02-09 09:50:12 +0530

    add React Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="login" element={<h1>Login</h1>} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
