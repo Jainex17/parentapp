e0d0086184e5ad185d88ac63670558dcd879e5db
Daksh <ozadaksh31@gmail.com>
2024-02-21 18:31:12 +0530

    functions have been changed, get requests implemented

--- recommendation-engine/functions.py
+++ recommendation-engine/functions.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import json
 import joblib
+import requests
 from scipy.sparse import csr_matrix
 from itertools import zip_longest
 from sklearn.neighbors import NearestNeighbors
@@ -26,13 +27,17 @@ def json_to_df(all_posts_json, all_users_json):
     for post in all_posts_json:
         post_id = post["postid"]
         total_like_count = post["like"]["total"]
-        likes = post["like"]["usrnames"]
-
-        for liked_user in likes:
-            collab_df_data.append({"post_id": post_id, "total_like_count": total_like_count, "liked": 1, "user_id": liked_user})
-
-        overview = " ".join(post["tags"] + [post["disc"], post["title"]])
-        content_df_data.append({"post_id": post_id, "total_like_count": total_like_count, "overview": overview})
+        try:
+            likes = post["like"]["usrnames"]
+        except Exception as e:
+            pass
+        else:
+            if likes != ['']:
+                for liked_user in likes:
+                    collab_df_data.append({"post_id": post_id, "total_like_count": total_like_count, "liked": 1, "user_id": liked_user})
+        finally:
+            overview = " ".join(post["tags"] + [post["disc"], post["title"]])
+            content_df_data.append({"post_id": post_id, "total_like_count": total_like_count, "overview": overview})
 
     collab_df = pd.DataFrame(collab_df_data, columns=["post_id", "total_like_count", "liked", "user_id"])
     content_df = pd.DataFrame(content_df_data, columns=["post_id", "total_like_count", "overview"])
@@ -135,22 +140,25 @@ def content_based_filtering(recently_liked_posts, sig, indices, df,  unseen_post
     
     scored_post_list = []
 
-    for post in recently_liked_posts:
-        idx = indices[post]
-        sig_scores = enumerate(sig[idx])
-        sig_scores = sorted(sig_scores, key=lambda x: x[1], reverse=True)
-        sig_scores = sig_scores[1:6]
-        posts = [(df['post_id'].iloc[i], score) for i, score in sig_scores]
-        scored_post_list.extend(posts)
-    
-    unseen_posts_set = set(unseen_posts)
+    if recently_liked_posts != ['']:
+        for post in recently_liked_posts:
+            idx = indices[post]
+            sig_scores = enumerate(sig[idx])
+            sig_scores = sorted(sig_scores, key=lambda x: x[1], reverse=True)
+            sig_scores = sig_scores[1:6]
+            posts = [(df['post_id'].iloc[i], score) for i, score in sig_scores]
+            scored_post_list.extend(posts)
+        
+        unseen_posts_set = set(unseen_posts)
 
-    scored_df = pd.DataFrame(scored_post_list, columns=['post_id', 'score'])
-    scored_df = scored_df.drop_duplicates(subset='post_id')
+        scored_df = pd.DataFrame(scored_post_list, columns=['post_id', 'score'])
+        scored_df = scored_df.drop_duplicates(subset='post_id')
 
-    # Sort and filter unseen posts
-    top_m_post_ids = scored_df.sort_values(by='score', ascending=False)['post_id'].head(n).tolist()
-    post_recommendation_list = [post_id for post_id in top_m_post_ids if post_id in unseen_posts_set]
+        # Sort and filter unseen posts
+        top_m_post_ids = scored_df.sort_values(by='score', ascending=False)['post_id'].head(n).tolist()
+        post_recommendation_list = [post_id for post_id in top_m_post_ids if post_id in unseen_posts_set]
+    else:
+        post_recommendation_list = []
 
     return post_recommendation_list
 
@@ -160,20 +168,23 @@ def collaborative_filtering(recently_liked_posts, knn_model, post_user_df, unsee
 
     scored_post_list = []
 
-    for post in recently_liked_posts:
-        post_index =post_user_df.index.get_loc(post)
-        distances, indices = knn_model.kneighbors(post_user_df.iloc[post_index,:].values.reshape(1, -1), n_neighbors = 6)
-        for i in range(1, len(distances.flatten())):
-            scored_post_list.append((post_user_df.index[indices.flatten()[i]], distances.flatten()[i]))
+    if recently_liked_posts != ['']:
+        for post in recently_liked_posts:
+            post_index =post_user_df.index.get_loc(post)
+            distances, indices = knn_model.kneighbors(post_user_df.iloc[post_index,:].values.reshape(1, -1), n_neighbors = 6)
+            for i in range(1, len(distances.flatten())):
+                scored_post_list.append((post_user_df.index[indices.flatten()[i]], distances.flatten()[i]))
 
-    unseen_posts_set = set(unseen_posts)
+        unseen_posts_set = set(unseen_posts)
 
-    scored_df = pd.DataFrame(scored_post_list, columns=['post_id', 'score'])
-    scored_df = scored_df.drop_duplicates(subset='post_id')
+        scored_df = pd.DataFrame(scored_post_list, columns=['post_id', 'score'])
+        scored_df = scored_df.drop_duplicates(subset='post_id')
 
-    # Sort and filter unseen posts
-    top_m_post_ids = scored_df.sort_values(by='score', ascending=True)['post_id'].head(m).tolist()
-    post_recommendation_list = [post_id for post_id in top_m_post_ids if post_id in unseen_posts_set]
+        # Sort and filter unseen posts
+        top_m_post_ids = scored_df.sort_values(by='score', ascending=True)['post_id'].head(m).tolist()
+        post_recommendation_list = [post_id for post_id in top_m_post_ids if post_id in unseen_posts_set]
+    else:
+        post_recommendation_list = []
 
     return post_recommendation_list
 
@@ -194,7 +205,9 @@ def mixing(internal_content_based_list, external_content_based_list, collaborati
 
 # api will send call to 2 functions - user_recommendation and every_n_hours
 
-def user_recommendation(user_json):
+def user_recommendation(rec_user):
+    user_json = requests.get(url = f"https://kadia.pythonanywhere.com/api/getusr?username={rec_user}&client=kadia").json()
+
     with open(os.path.join(script_dir,'all_posts_json_dump.json'), 'r') as json_file:
         all_post_json_list = json.load(json_file)
 
@@ -245,17 +258,20 @@ def user_recommendation(user_json):
 
     return final_post_list
 
-def every_n_hours(all_post_json_list, all_user_json_list):
+def every_n_hours():
+    all_post_json_list_raw = requests.get(url = "https://kadia.pythonanywhere.com/api/getallposts?client=kadia").content
+    all_post_json_list = json.loads(all_post_json_list_raw)
+    for post in all_post_json_list:
+        post['title'] = post['title'].encode('unicode-escape').decode('utf-8')
+        post['disc'] = post['disc'].encode('unicode-escape').decode('utf-8')
+
+    all_user_json_list = requests.get(url="https://kadia.pythonanywhere.com/api/getallusers?client=kadia").json()
+
     collab_df, content_df = json_to_df(all_posts_json = all_post_json_list, all_users_json=all_user_json_list)
     post_user_dataset_model(collab_df)
     post_dataset(content_df)
     return True
 
-# test data
-all_post_json_list = [{'title': 'my second post new here', 'disc': 'my second post discription', 'usrname': 'datauser1', 'usrid': 'N_usr_31', 'postid': 'U_post_15', 'ptype': 'post', 'tags': ['unmarried'], 'like': {'total': 2, 'usrnames': ['datauser1', 'datauser3']}, 'comment': {'comments': [''], 'total': 0, 'usrnames': ['']}, 'timestamp': '20/02/2024_22:48:54', 'url': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/post_images%2FU_post_15?alt=media'}, {'title': 'my first post new here', 'disc': 'my first post discription', 'usrname': 'datauser1', 'usrid': 'N_usr_31', 'postid': 'U_post_14', 'ptype': 'post', 'tags': ['single'], 'like': {'total': 4, 'usrnames': ['datauser1', 'datauser3', 'datauser2', 'datauser4']}, 'comment': {'comments': [''], 'total': 0, 'usrnames': ['']}, 'timestamp': '20/02/2024_22:48:32', 'url': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/post_images%2FU_post_14?alt=media'}, {'title': 'my second post new here', 'disc': 'my second post discription', 'usrname': 'datauser2', 'usrid': 'N_usr_32', 'postid': 'U_post_17', 'ptype': 'post', 'tags': ['married', 'teenchild'], 'like': {'total': 1, 'usrnames': ['datauser1']}, 'comment': {'comments': [''], 'total': 0, 'usrnames': ['']}, 'timestamp': '20/02/2024_22:50:50', 'url': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/post_images%2FU_post_17?alt=media'}, {'title': 'my first post new here', 'disc': 'my second post discription', 'usrname': 'datauser2', 'usrid': 'N_usr_32', 'postid': 'U_post_16', 'ptype': 'post', 'tags': ['married', 'singlechild'], 'like': {'total': 2, 'usrnames': ['datauser5', 'datauser3']}, 'comment': {'comments': [''], 'total': 0, 'usrnames': ['']}, 'timestamp': '20/02/2024_22:50:34', 'url': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/post_images%2FU_post_16?alt=media'}, {'title': 'why my child has red rashes?', 'disc': 'my child discription', 'usrname': 'datauser3', 'usrid': 'N_usr_33', 'postid': 'U_post_19', 'ptype': 'question', 'tags': ['married', 'rashes', '2child'], 'like': {'total': 5, 'usrnames': ['datauser1', 'datauser3','datauser2', 'datauser4','datauser5']}, 'comment': {'comments': [''], 'total': 0, 'usrnames': ['']}, 'timestamp': '20/02/2024_22:52:34', 'url': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/post_images%2FU_post_19?alt=media'}, {'title': 'why my child has fever?', 'disc': 'my child discription', 'usrname': 'datauser3', 'usrid': 'N_usr_33', 'postid': 'U_post_18', 'ptype': 'question', 'tags': ['married', 'fever', '2child'], 'like': {'total': 2, 'usrnames': ['datauser1', 'datauser4']}, 'comment': {'comments': [''], 'total': 0, 'usrnames': ['']}, 'timestamp': '20/02/2024_22:52:18', 'url': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/post_images%2FU_post_18?alt=media'}, {'title': 'lovely smile', 'disc': 'my child discription', 'usrname': 'datauser4', 'usrid': 'N_usr_34', 'postid': 'U_post_21', 'ptype': 'post', 'tags': ['married', 'infant', 'singlechild'], 'like': {'total': 1, 'usrnames': ['datauser5']}, 'comment': {'comments': [''], 'total': 0, 'usrnames': ['']}, 'timestamp': '20/02/2024_22:54:09', 'url': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/post_images%2FU_post_21?alt=media'}, {'title': 'why my child not eating?', 'disc': 'my child discription', 'usrname': 'datauser4', 'usrid': 'N_usr_34', 'postid': 'U_post_20', 'ptype': 'question', 'tags': ['married', 'infant'], 'like': {'total': 2, 'usrnames': ['datauser4', 'datauser5']}, 'comment': {'comments': [''], 'total': 0, 'usrnames': ['']}, 'timestamp': '20/02/2024_22:53:46', 'url': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/post_images%2FU_post_20?alt=media'}, {'title': 'how to stop porn addiction of my child?', 'disc': 'my child discription', 'usrname': 'datauser5', 'usrid': 'N_usr_35', 'postid': 'U_post_23', 'ptype': 'question', 'tags': ['unmarried', 'teenchild', 'housewife'], 'like': {'total': 3, 'usrnames': ['datauser1', 'datauser3', 'datauser4']}, 'comment': {'comments': [''], 'total': 0, 'usrnames': ['']}, 'timestamp': '20/02/2024_22:55:52', 'url': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/post_images%2FU_post_23?alt=media'}, {'title': 'lovely child`', 'disc': 'my child discription', 'usrname': 'datauser5', 'usrid': 'N_usr_35', 'postid': 'U_post_22', 'ptype': 'post', 'tags': ['un`married', 'teenchild'], 'like': {'total': 2, 'usrnames': ['datauser4', 'datauser5']}, 'comment': {'comments': [''], 'total': 0, 'usrnames': ['']}, 'timestamp': '20/02/2024_22:55:19', 'url': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/post_images%2FU_post_22?alt=media'}]
-all_user_json_list = [{'usrname': 'datauser1', 'passwd': 'datauser1123', 'id': 'N_usr_31', 'hash': 'eaea235b965aacc92239af0672f50e8d', 'email': 'datauser1@gmail.com', 'age': 20, 'gender': 'male', 'tags': ['unmarried', 'single'], 'bio': 'my new bio for this profile', 'profession': 'teacher', 'posts': ['', 'U_post_14', 'U_post_15'], 'pimg': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/profile_images%2FN_usr_31?alt=media', 'likedposts': [''], 'followers': [''], 'following': [''], 'followingcount': 0, 'followerscount': 0}, {'usrname': 'datauser2', 'passwd': 'datauser2123', 'id': 'N_usr_32', 'hash': '168ffd39765e941442a13a0cef2c2a05', 'email': 'datauser2@gmail.com', 'age': 20, 'gender': 'male', 'tags': ['married', '1child', 'teenchild'], 'bio': 'my new bio for this profile', 'profession': 'teacher', 'posts': ['', 'U_post_16', 'U_post_17'], 'pimg': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/profile_images%2FN_usr_32?alt=media', 'likedposts': [''], 'followers': [''], 'following': [''], 'followingcount': 0, 'followerscount': 0}, {'usrname': 'datauser3', 'passwd': 'datauser3123', 'id': 'N_usr_33', 'hash': 'b23b55bd24ed7063c70f76ce2ad0bced', 'email': 'datauser3@gmail.com', 'age': 20, 'gender': 'female', 'tags': ['married', '2child'], 'bio': 'my new bio for this profile', 'profession': 'doctor', 'posts': ['', 'U_post_18', 'U_post_19'], 'pimg': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/profile_images%2FN_usr_33?alt=media', 'likedposts': [''], 'followers': [''], 'following': [''], 'followingcount': 0, 'followerscount': 0}, {'usrname': 'datauser4', 'passwd': 'datauser4123', 'id': 'N_usr_34', 'hash': '2fba1b465931a69e8a958cf2dd72dffa', 'email': 'datauser4@gmail.com', 'age': 20, 'gender': 'female', 'tags': ['married', 'singlechild', 'infant'], 'bio': 'my new bio for this profile', 'profession': 'engineer', 'posts': ['', 'U_post_20', 'U_post_21'], 'pimg': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/profile_images%2FN_usr_34?alt=media', 'likedposts': [''], 'followers': [''], 'following': [''], 'followingcount': 0, 'followerscount': 0}, {'usrname': 'datauser5', 'passwd': 'datauser5123', 'id': 'N_usr_35', 'hash': '1197a4182ed02368315e14f739ea30e7', 'email': 'datauser5@gmail.com', 'age': 20, 'gender': 'female', 'tags': ['unmarried', 'singlechild', 'teenchild'], 'bio': 'my new bio for this profile', 'profession': 'housewife', 'posts': ['', 'U_post_22', 'U_post_23'], 'pimg': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/profile_images%2FN_usr_35?alt=media', 'likedposts': [''], 'followers': [''], 'following': [''], 'followingcount': 0, 'followerscount': 0}]
-user_json = {'usrname': 'datauser1', 'passwd': 'datauser1123', 'id': 'N_usr_31', 'hash': 'eaea235b965aacc92239af0672f50e8d', 'email': 'datauser1@gmail.com', 'age': 20, 'gender': 'male', 'tags': ['unmarried', 'single'], 'bio': 'my new bio for this profile', 'profession': 'teacher', 'posts': ['', 'U_post_14', 'U_post_15'], 'pimg': 'https://firebasestorage.googleapis.com/v0/b/parentapp-df60c.appspot.com/o/profile_images%2FN_usr_31?alt=media', 'likedposts': ['U_post_18', 'U_post_19'], 'followers': [''], 'following': [''], 'followingcount': 0, 'followerscount': 0}
+every_n_hours()
 
-every_n_hours(all_post_json_list, all_user_json_list)
-
-print(user_recommendation(user_json=user_json))
+user_recommendation(rec_user="apifinaltest3")
